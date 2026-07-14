c90740c41fa717155cf025e950d332c26a54ca7c
freyder montano <fremontano24@gmail.com>
2026-07-13 21:16:35 -0400

    feat: ajuste llamada a la api

--- src/api/coincap.ts
+++ src/api/coincap.ts
@@ -5,6 +5,6 @@ console.log("KEY:", import.meta.env.VITE_API_KEY);
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    Authorization: ` ${import.meta.env.VITE_API_KEY}`,
+    Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
   },
 });
